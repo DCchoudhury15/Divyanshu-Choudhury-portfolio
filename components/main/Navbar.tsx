2f5877014a0bd4a10d8ff5133bdf3d771bad6163
Divyanshu Choudhury <divyanshuchoudhury3@gmail.com>
2025-06-22 00:56:41 +0530

    improved-changes-1

--- components/main/Navbar.tsx
+++ components/main/Navbar.tsx
@@ -7,7 +7,7 @@ const Navbar = () => {
     <div className="w-full h-[65px] fixed top-0 shadow-lg shadow-[#2A0E61]/50 bg-[#03001417] backdrop-blur-md z-50 px-10">
       <div className="w-full h-full flex flex-row items-center justify-between m-auto px-[10px]">
         <a
-          href="#about-me"
+          href="#aboutme"
           className="h-auto w-auto flex flex-row items-center"
         >
           <Image
@@ -19,13 +19,13 @@ const Navbar = () => {
           />
 
           <span className="font-bold ml-[10px] hidden md:block text-gray-300">
-            WebChain Dev
+            My Portfolio
           </span>
         </a>
 
         <div className="w-[500px] h-full flex flex-row items-center justify-between md:mr-20">
           <div className="flex items-center justify-between w-full h-auto border border-[#7042f861] bg-[#0300145e] mr-[15px] px-[20px] py-[10px] rounded-full text-gray-200">
-            <a href="#about-me" className="cursor-pointer">
+            <a href="#aboutme" className="cursor-pointer">
               About me
             </a>
             <a href="#skills" className="cursor-pointer">
@@ -39,6 +39,12 @@ const Navbar = () => {
 
         <div className="flex flex-row gap-5">
           {Socials.map((social) => (
+            <a
+            key={social.name}
+      href={social.link}
+      target="_blank"
+      rel="noopener noreferrer"
+    >
             <Image
               src={social.src}
               alt={social.name}
@@ -46,6 +52,7 @@ const Navbar = () => {
               width={24}
               height={24}
             />
+            </a>
           ))}
         </div>
       </div>
--- components/main/StarBackground.tsx
+++ components/main/StarBackground.tsx
@@ -1,7 +1,7 @@
 "use client"
 import React, {useState, useRef, Suspense} from 'react';
 import {Canvas, useFrame}from '@react-three/fiber'
-import {Points, PointMaterial, Preload} from '@react-three/drei'
+import {Points, PointMaterial} from '@react-three/drei'
 //@ts-ignore
 import * as random from 'maath/random/dist/maath-random.esm'
 
@@ -37,15 +37,12 @@ const StarBackground = (props:any) => {
 };
 
 const StarCanvas=()=>(
-<div className='w-full h-auto fixed inset-0 z-[20]'>
+<div className='w-full h-auto fixed inset-0 z-0 pointer-events-none'>
     <Canvas camera={{position:[0,0,1]}}>
     <Suspense fallback={null}>
         <StarBackground/>
     </Suspense>
-    
-
     </Canvas>
-
 </div>
 )
 
